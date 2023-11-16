31cdb688dd74078320b1c348967a4935d3aa68fc
Riku <79360452+rikuvunua@users.noreply.github.com>
2023-11-16 15:05:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var container = document.querySelector('.container');
+var viewHeight = document.documentElement.clientHeight;
+var pageNum = document.querySelectorAll('.section').length;
+
+var currentPosition = 0;
+
+container.style.height = viewHeight + 'px';
+
+function goDown () {
+  if (currentPosition > - viewHeight * (pageNum - 1)) {
+    currentPosition = currentPosition - viewHeight;
+    container.style.top = currentPosition + 'px';
+  }
+}
+
+function goUp () {
+  if (currentPosition < 0) {
+    currentPosition = currentPosition + viewHeight;
+    container.style.top = currentPosition + 'px';
+  }
+}
+
+function throttle (fn, delay) {
+  let baseTime = 0;
+  return function () {
+    const currentTime = Date.now();
+    if (baseTime + delay < currentTime) {
+      fn.apply(this, arguments);
+      baseTime = currentTime;
+    }
+  }
+}
+
+var handlerWheel = throttle(scrollMove, 1000);
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousewheel_event#The_detail_property
+if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
+  document.addEventListener('mousewheel', handlerWheel);
+} else {
+  document.addEventListener('DOMMouseScroll', handlerWheel);
+}
+function scrollMove (e) {
+  if (e.deltaY > 0) {
+    goDown();
+  } else {
+    goUp();
+  }
+}
+
+var touchStartY = 0;
+document.addEventListener('touchstart', event => {
+  touchStartY = event.touches[0].pageY;
+})
+var handleTouchEnd = throttle(touchEnd, 500);
+document.addEventListener('touchend', handleTouchEnd);
+function touchEnd (e) {
+  var touchEndY = e.changedTouches[0].pageY;
+  if (touchEndY - touchStartY < 0) {
+    goDown();
+  } else {
+    goUp();
+  }
+}
