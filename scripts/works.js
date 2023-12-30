a4a1eaa734d364230414494cce550e2249c1d21b
Yoshiharunrunrunko <153973018+Yoshiharunrunrunko@users.noreply.github.com>
2023-12-31 01:24:14 +0900

    大体終わったslickもうこれでいい・・？？？なんか上手くいかないんだていうかメニュー消えたんだけど助けてくれ

---
+++ scripts/works.js
@@ -0,0 +1,18 @@
+$(".slide-items").slick({
+  autoplay: true,
+  slidesToShow: 3, // デスクトップ時の表示枚数
+  infinite: true,
+  slidesToScroll: 1,
+  dots: true,
+  initialSlide: 0,
+
+
+  responsive: [
+    {
+      breakpoint: 844, // モバイルのブレークポイント
+      settings: {
+        slidesToShow: 2, // モバイル時の表示枚数
+      },
+    },
+  ],
+});
