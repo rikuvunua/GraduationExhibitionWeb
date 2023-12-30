ac715d51b77be92b90d6eb3d62e8bcd07c5cce7c
rikuvunua <rikuvunua@gmail.com>
2023-12-30 19:55:12 +0900

    スクロールする方法をリニューアルした
    修正したエラー：
    1. モバイルの画面でメニューを起動すると前のページに戻る
    2. モバイルの画面でタップすると自動的に前のページに戻る
    
    スクロールする方法を変更した

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,59 @@
-var container = document.querySelector('.container');
-var viewHeight = document.documentElement.clientHeight;
-var pageNum = document.querySelectorAll('.section').length;
+let isScrolling = false;
 
-var currentPosition = 0;
+window.addEventListener('wheel', function (e) {
+  e.preventDefault();
 
-container.style.height = viewHeight + 'px';
+  if (!isScrolling) {
+    isScrolling = true;
 
-function goDown () {
-  if (currentPosition > - viewHeight * (pageNum - 1)) {
-    currentPosition = currentPosition - viewHeight;
-    container.style.top = currentPosition + 'px';
-  }
-}
-
-function goUp () {
-  if (currentPosition < 0) {
-    currentPosition = currentPosition + viewHeight;
-    container.style.top = currentPosition + 'px';
-  }
-}
-
-function throttle (fn, delay) {
-  let baseTime = 0;
-  return function () {
-    const currentTime = Date.now();
-    if (baseTime + delay < currentTime) {
-      fn.apply(this, arguments);
-      baseTime = currentTime;
+    if (e.deltaY < 0) {
+      window.scrollBy({
+        top: -window.innerHeight,
+        behavior: 'smooth'
+      });
+    } else {
+      window.scrollBy({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      });
     }
-  }
-}
 
-var handlerWheel = throttle(scrollMove, 1000);
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousewheel_event#The_detail_property
-if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
-  document.addEventListener('mousewheel', handlerWheel);
-} else {
-  document.addEventListener('DOMMouseScroll', handlerWheel);
-}
-function scrollMove (e) {
-  if (e.deltaY > 0) {
-    goDown();
-  } else {
-    goUp();
+    setTimeout(function () {
+      isScrolling = false;
+    }, 1000);
   }
-}
+}, { passive: false });
 
-var touchStartY = 0;
-document.addEventListener('touchstart', event => {
-  touchStartY = event.touches[0].pageY;
-})
-var handleTouchEnd = throttle(touchEnd, 500);
-document.addEventListener('touchend', handleTouchEnd);
-function touchEnd (e) {
-  var touchEndY = e.changedTouches[0].pageY;
-  if (touchEndY - touchStartY < 0) {
-    goDown();
-  } else {
-    goUp();
+let touchStartY = 0;
+let touchEndY = 0;
+
+window.addEventListener('touchstart', function (e) {
+  touchStartY = e.changedTouches[0].screenY;
+}, false);
+
+window.addEventListener('touchend', function (e) {
+  touchEndY = e.changedTouches[0].screenY;
+  handleSwipeGesture();
+}, false);
+
+function handleSwipeGesture() {
+  if (touchEndY < touchStartY) {
+    window.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
+  if (touchEndY > touchStartY) {
+    window.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth'
+    });
   }
 }
 
 // FadeIn
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const backgroundFadeInElement = document.querySelector('.fade-in');
   backgroundFadeInElement.classList.add('active');
 
-  container.style.height = '1080px';
-  container.style.top = '-1080px';
 });
-
